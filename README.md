1f0cfc119594c803f0e950ea8a70d2dbc0ac0b45
soomjeet.sahoo <soomjeet.sahoo@salesforce.com>
2023-08-05 07:35:21 +0530

    Added dependencies

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JWTStreamerOnJS
+
+# To install all dependencies, please run the below command
+
+```node:
+npm install dotenv axios fs jsforce jsonwebtoken moment querystring url
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+var axios = require('axios');
+var fs = require('fs');
+var jsforce = require('jsforce');
+var jwt = require('jsonwebtoken');
+var moment = require('moment');
+var querystring = require('querystring');
+var url = require('url');
+
